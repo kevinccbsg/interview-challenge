f870659528f7ce1870344d18239917993d698609
kevinccbsg <kevinccbsg@gmail.com>
2023-01-17 23:34:01 +0100

    add index logic

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const validate = require('./schema/validate')
+const validate = require('./schema/validate');
+const { removeIDs, removeJSONDuplicates } = require('./utils');
 
 const cleanApp = (appObject) => {
   if (!appObject) {
     throw new Error('Missing App object');
   }
   validate(appObject);
-  return true;
+  const versions = appObject.versions.map(version => {
+    const filteredObjects = removeJSONDuplicates(version.objects);
+    const scenes = version.scenes.map(scene => {
+      const filteredViews = removeJSONDuplicates(scene.views);
+      return { ...scene, views: filteredViews };
+    });
+    return {
+      ...version,
+      objects: filteredObjects,
+      scenes,
+    };
+  });
+  return { ...appObject, versions };
 };
 
 module.exports = cleanApp;
